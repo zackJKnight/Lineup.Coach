7f96b3ffff4236a7674c790f708a924b628bbabd
zackJKnight <zachary.j.knight@gmail.com>
2019-06-15 18:05:09 -0500

    Add Game controller.

---
+++ LineupCoach.App/Controllers/GamesController.cs
@@ -0,0 +1,21 @@
+﻿using Lineup.Coach.Application.Games.Commands.CreateGame;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LineupCoach.App.Controllers
+{
+    public class GamesController : BaseController
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Create([FromBody]CreateGameCommand command)
+        {
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+}
