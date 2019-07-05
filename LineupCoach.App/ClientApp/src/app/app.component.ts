7906ba1a5aae188f5d64459c7440434ffc6de544
ftc10179classuser <zachary.j.knight@gmail.com>
2019-07-05 09:03:52 -0400

    Add Angular app

---
+++ LineupCoach.App/ClientApp/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AddTeamComponent } from './teams/add-team/add-team.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'Lineup';
+
+}
