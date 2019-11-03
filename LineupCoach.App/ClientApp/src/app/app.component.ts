c61c68bbba15c5f3d7d1e84444f9e91196ced5ac
zackJKnight <zachary.j.knight@gmail.com>
2019-11-02 19:00:40 -0500

    remove unused component

--- LineupCoach.App/ClientApp/src/app/app.component.ts
+++ LineupCoach.App/ClientApp/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { AddTeamComponent } from './teams/add-team/add-team.component';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
